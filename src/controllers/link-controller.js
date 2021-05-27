cec89142760dd7f8af7beb5d77136d33047f1fb5
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-05-27 09:26:00 -0300

    :sparkles: get all links

--- src/controllers/link-controller.js
+++ src/controllers/link-controller.js
@@ -1,6 +1,11 @@
 const Link = require('../models/link')
 
 module.exports = {
+  index: async (req, res) => {
+    const links = await Link.findAll()
+
+    res.json(links)
+  },
   store: async (req, res) => {
     const { title, url } = req.body
     const link = await Link.create({ title, url })
--- src/routes/links.js
+++ src/routes/links.js
@@ -2,6 +2,7 @@ const express = require('express')
 const LinkController = require('../controllers/link-controller')
 const router = express.Router()
 
+router.get('/', LinkController.index)
 router.post('/', LinkController.store)
 
 module.exports = router
