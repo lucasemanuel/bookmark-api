404926e61be58b0539e3bc994db03c5f3390462d
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-05-30 22:40:16 -0300

    :wrench: update sequelize config

--- src/config/database.js
+++ src/config/database.js
@@ -3,13 +3,26 @@ require('dotenv').config()
 const { DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_HOST } = process.env
 
 module.exports = {
-  database: DB_DATABASE,
-  username: DB_USERNAME,
-  password: DB_PASSWORD,
-  dialect: 'mysql',
-  host: DB_HOST,
-  define: {
-    timestamps: true,
-    underscored: true
+  development: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    host: DB_HOST,
+    port: 3306,
+    dialect: 'mysql'
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql'
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'mysql'
   }
 }
