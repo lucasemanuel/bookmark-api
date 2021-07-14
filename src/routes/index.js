770858d5b4ca3e854f62ad660a89cb6438d1a45c
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-07-14 15:52:59 -0300

    :fire: clearing project

--- src/routes/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router()
-
-router.use('/links', require('./links'))
-router.use('/tags', require('./tags'))
-
-module.exports = router
--- src/validators/tag-validator.js
+++
@@ -1,9 +0,0 @@
-const { body } = require('express-validator')
-
-module.exports = {
-  store: [
-    body('name')
-      .isLength({ max: 255 })
-      .notEmpty()
-  ]
-}
